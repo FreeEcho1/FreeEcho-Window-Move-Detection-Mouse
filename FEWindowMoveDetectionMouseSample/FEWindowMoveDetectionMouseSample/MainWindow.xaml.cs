81f20596a0ae60b9b74bc35607c6f3eaa5168f66
FreeEcho <20364419+FreeEcho1@users.noreply.github.com>
2022-06-26 09:49:13 +0900

    Add files via upload

--- FEWindowMoveDetectionMouseSample/FEWindowMoveDetectionMouseSample/MainWindow.xaml.cs
+++ FEWindowMoveDetectionMouseSample/FEWindowMoveDetectionMouseSample/MainWindow.xaml.cs
@@ -1,116 +1,115 @@
-﻿namespace FEWindowMoveDetectionMouseSample
+﻿namespace FEWindowMoveDetectionMouseSample;
+
+public partial class MainWindow : System.Windows.Window
 {
-    public partial class MainWindow : System.Windows.Window
+    private readonly FreeEcho.FEWindowMoveDetectionMouse.MouseMoveWindowDetection MouseMoveWindowDetection = new();
+    private int Count = 0;
+
+    public MainWindow()
     {
-        private FreeEcho.FEWindowMoveDetectionMouse.MouseMoveWindowDetection MouseMoveWindowDetection = new();
-        private int Count = 0;
+        InitializeComponent();
 
-        public MainWindow()
+        try
         {
-            InitializeComponent();
-
-            try
-            {
-                Loaded += MainWindow_Loaded;
-                Closed += MainWindow_Closed;
-                MouseMoveWindowDetection.StartMove += MouseMoveWindowDetection_StartMove;
-                MouseMoveWindowDetection.Moved += MouseMoveWindowDetection_Moved;
-                MouseMoveWindowDetection.StopMove += MouseMoveWindowDetection_StopMove;
-            }
-            catch
-            {
-            }
+            Loaded += MainWindow_Loaded;
+            Closed += MainWindow_Closed;
+            MouseMoveWindowDetection.StartMove += MouseMoveWindowDetection_StartMove;
+            MouseMoveWindowDetection.Moved += MouseMoveWindowDetection_Moved;
+            MouseMoveWindowDetection.StopMove += MouseMoveWindowDetection_StopMove;
         }
-
-        private void MainWindow_Loaded(
-            object sender,
-            System.Windows.RoutedEventArgs e
-            )
+        catch
         {
-            try
-            {
-                MouseMoveWindowDetection.Start();
-            }
-            catch
-            {
-            }
         }
+    }
 
-        private void MainWindow_Closed(
-            object sender,
-            System.EventArgs e
-            )
+    private void MainWindow_Loaded(
+        object sender,
+        System.Windows.RoutedEventArgs e
+        )
+    {
+        try
         {
-            try
-            {
-                MouseMoveWindowDetection.Stop();
-            }
-            catch
-            {
-            }
+            MouseMoveWindowDetection.Start();
         }
-
-        /// <summary>
-        /// マウスでウィンドウ移動の「StartMove」イベント
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void MouseMoveWindowDetection_StartMove(
-            object sender,
-            FreeEcho.FEWindowMoveDetectionMouse.StartMoveEventArgs e
-            )
+        catch
         {
-            try
-            {
-                EventTextBox.Text += Count + " : StartMove\n";
-                EventTextBox.ScrollToEnd();
-                Count++;
-            }
-            catch
-            {
-            }
         }
+    }
 
-        /// <summary>
-        /// マウスでウィンドウ移動の「Moved」イベント
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void MouseMoveWindowDetection_Moved(
-            object sender,
-            FreeEcho.FEWindowMoveDetectionMouse.MoveEventArgs e
-            )
+    private void MainWindow_Closed(
+        object sender,
+        System.EventArgs e
+        )
+    {
+        try
         {
-            try
-            {
-                EventTextBox.Text += Count + " : Moved\n";
-                EventTextBox.ScrollToEnd();
-                Count++;
-            }
-            catch
-            {
-            }
+            MouseMoveWindowDetection.Stop();
         }
-
-        /// <summary>
-        /// マウスでウィンドウ移動の「StopMove」イベント
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void MouseMoveWindowDetection_StopMove(
-            object sender,
-            FreeEcho.FEWindowMoveDetectionMouse.StopMoveEventArgs e
-            )
+        catch
         {
-            try
-            {
-                EventTextBox.Text += Count + " : StopMove\n";
-                EventTextBox.ScrollToEnd();
-                Count++;
-            }
-            catch
-            {
-            }
+        }
+    }
+
+    /// <summary>
+    /// マウスでウィンドウ移動の「StartMove」イベント
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void MouseMoveWindowDetection_StartMove(
+        object sender,
+        FreeEcho.FEWindowMoveDetectionMouse.StartMoveEventArgs e
+        )
+    {
+        try
+        {
+            EventTextBox.Text += Count + " : StartMove\n";
+            EventTextBox.ScrollToEnd();
+            Count++;
+        }
+        catch
+        {
+        }
+    }
+
+    /// <summary>
+    /// マウスでウィンドウ移動の「Moved」イベント
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void MouseMoveWindowDetection_Moved(
+        object sender,
+        FreeEcho.FEWindowMoveDetectionMouse.MoveEventArgs e
+        )
+    {
+        try
+        {
+            EventTextBox.Text += Count + " : Moved\n";
+            EventTextBox.ScrollToEnd();
+            Count++;
+        }
+        catch
+        {
+        }
+    }
+
+    /// <summary>
+    /// マウスでウィンドウ移動の「StopMove」イベント
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void MouseMoveWindowDetection_StopMove(
+        object sender,
+        FreeEcho.FEWindowMoveDetectionMouse.StopMoveEventArgs e
+        )
+    {
+        try
+        {
+            EventTextBox.Text += Count + " : StopMove\n";
+            EventTextBox.ScrollToEnd();
+            Count++;
+        }
+        catch
+        {
         }
     }
 }
